4fa6364a1a15c99b1016a27f15cccc35c94be69b
Matvey Khvatov <matveykhvatov@gmail.com>
2020-08-11 00:00:13 +0300

    Added Facade

---
+++ facade/main.py
@@ -0,0 +1,53 @@
+from zeep import (
+    Client,
+    exceptions,
+)
+
+
+class Calculator:
+    """Facade class for calculator web service (http://www.dneonline.com/calculator.asmx)"""
+
+    WSDL = 'http://www.dneonline.com/calculator.asmx?WSDL'
+    client = Client(wsdl=WSDL).service
+
+    @staticmethod
+    def _calculate(a: int, b: int, command):
+        """command: client with operation name from wsdl file;
+        for example: self.client.Add"""
+
+        request_params = {
+            'intA': a,
+            'intB': b,
+        }
+
+        try:
+            return command(**request_params)
+        except exceptions.Fault as error:
+            print(error.message)
+
+    def add(self, a: int, b: int) -> int:
+        """Adds two numbers"""
+        return self._calculate(a, b, command=self.client.Add)
+
+    def divide(self, a: int, b: int) -> int:
+        """Divides number a by number b"""
+        return self._calculate(a, b, command=self.client.Divide)
+
+    def multiply(self, a: int, b: int) -> int:
+        """Multiplies two numbers"""
+        return self._calculate(a, b, command=self.client.Multiply)
+
+    def subtract(self, a: int, b: int) -> int:
+        """Subtracts b from a"""
+        return self._calculate(a, b, command=self.client.Subtract)
+
+
+if __name__ == "__main__":
+    # Client's code:
+
+    calculator = Calculator()
+
+    print(calculator.add(4, 5))  # 9
+    print(calculator.divide(10, 2))  # 5
+    print(calculator.multiply(5, 2))  # 10
+    print(calculator.subtract(7, 3))  # 4
